fde6e3c3f5ea4885c4bb267eaf765fe2835c1c78
iTzBoboCz <bobo12002@icloud.com>
2020-10-25 20:30:40 +0100

    difficulties, statistics about how many times letter was inputted in one word

--- Program.cs
+++ Program.cs
@@ -22,11 +22,63 @@ namespace hangman
 Made by Ondřej Pešek
 -----------------------------------------------
 ");
-      string[] words = File.ReadAllLines(@"word_list.txt");
+
+      if (!File.Exists(@"word_list.txt"))
+      {
+        Console.WriteLine("File with words doesn't exist. Creating one!");
+        using (StreamWriter writer = File.AppendText(@"word_list.txt"))
+        {
+          string[] wordsDefault = { "hat", "fish", "recipe", "emotion", "statement", "development" };
+          for (int i = 0; i < wordsDefault.Length; i++)
+          {
+            writer.WriteLine(wordsDefault[i]);
+          };
+        }
+        Console.WriteLine("Done!");
+        System.Threading.Thread.Sleep(2000);
+      }
+
+      string[] file = File.ReadAllLines(@"word_list.txt");
+      List<string> words = new List<string>();
+
+      string inputDifficulty = string.Empty;
+      int difficulty;
+
+      while (true)
+      {
+        do
+        {
+          Console.Write("Choose your difficulty:\n  [0] => easy\n  [1] => medium\n  [2] => expert\n: ");
+          inputDifficulty = Console.ReadLine();
+        } while (!int.TryParse(inputDifficulty, out difficulty));
+
+        if (difficulty < 0 || difficulty > 2 )
+        {
+          continue;   
+        }
+
+        break;
+      }
+
+      foreach (var word in file)
+      {
+        if ((word.Length >= 3 && word.Length <= 5) && difficulty == 0) words.Add(word);
+        else if ((word.Length >= 6 && word.Length <= 8) && difficulty == 1) words.Add(word);
+        else if (word.Length >= 9 && difficulty == 2) words.Add(word);
+      }
+
+      Dictionary<char, int> alphabet = new Dictionary<char, int>();
+
+      for (char i = 'a'; i < 'z'; i++)
+      {
+        alphabet.Add(i, 0);
+      }
+
+      // System.Environment.Exit(1);
 
       // https://developerslogblog.wordpress.com/2020/02/04/how-to-shuffle-an-array/
       Random random = new Random();
-      for (int i = words.Length - 1; i > 0; i--)
+      for (int i = words.Count - 1; i > 0; i--)
       {
         int randomIndex = random.Next(0, i + 1);
 
@@ -38,99 +90,111 @@ Made by Ondřej Pešek
       int won = 0;
       int lost = 0;
 
-      for (int i = 0; i < words.Length; i++)
+      for (int i = 0; i < words.Count; i++)
       {
         string word = words[i].ToLower();
         
-        int lives = words.Length; // počet pokusů = počet písmen
+        int lives = words.Count; // počet pokusů = počet písmen
         List<char> guessedLetters = new List<char>();
 
         while (true)
         {
-            if (lives < 1)
-            {
-              Console.Clear();
-
-              lost++;
-
-              Console.ForegroundColor = ConsoleColor.Red;
-              Console.WriteLine("You just lost!\nMaybe you'll win the next one!");
-              Console.ResetColor();
-
-              System.Threading.Thread.Sleep(1000);
-              break;
-            }
-
+          if (lives < 1)
+          {
             Console.Clear();
-            char input = '\0';
-            string output = String.Empty;
-            
-            Console.WriteLine($"{won} won | {lost} lost");
+
+            lost++;
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(new String((char)3, lives)); // (char)3 = ♥
+            Console.WriteLine("You just lost!\nMaybe you'll win the next one!");
             Console.ResetColor();
 
-            // existují již nějaká uhodnutá písmena?
-            if (!guessedLetters.Any())
+            System.Threading.Thread.Sleep(3000);
+            break;
+          }
+
+          Console.Clear();
+          char input = '\0';
+          string output = String.Empty;
+          
+          Console.WriteLine($"{won} won | {lost} lost");
+
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine(new String((char)3, lives)); // (char)3 = ♥
+          Console.ResetColor();
+
+          // existují již nějaká uhodnutá písmena?
+          if (!guessedLetters.Any())
+          {
+            output = new String('_', word.Length);
+          }
+          else
+          {
+            // print all guessed letters
+            Console.WriteLine("guessed letters: "+string.Join(", ", guessedLetters));
+
+            for (int e = 0; e < word.Length; e++)
             {
-              output = new String('_', word.Length);
+              if (guessedLetters.Contains(word[e]))
+              {
+                output += word[e];
+              } else
+              {
+                output += "_";
+              }
             }
-            else
+
+            if (output == word)
             {
-              // print all guessed letters
-              Console.WriteLine("guessed letters: "+string.Join(", ", guessedLetters));
-
-              for (int e = 0; e < word.Length; e++)
-              {
-                if (guessedLetters.Contains(word[e]))
-                {
-                  output += word[e];
-                } else
-                {
-                  output += "_";
-                }
-              }
-
-              if (output == word)
-              {
-                won++;
-                break;
-              }              
-            }
-
-            Console.WriteLine(output);
-
-            // hádání písmena
-            while (true)
-            {
-              Console.WriteLine(":");
-
-              // pokud je input validní písmeno
-              if (!char.TryParse(Console.ReadLine(), out input) && !char.IsLetter(input))
-              {
-                continue;
-              }
-
-              // převedení písmena na lowercase
-              input = char.ToLower(input);
-
-              // pokud hráč písmeno již nehádal
-              if (guessedLetters.Contains(input))
-              {
-                  continue;
-              }
-
-              // pokud hádané písmeno ve slově není, odebere 1 život
-              if (!word.Contains(input))
-              {
-                  lives--;
-              }
+              won++;
               break;
+            }              
+          }
+
+          Console.WriteLine(output);
+
+          // hádání písmena
+          while (true)
+          {
+            Console.Write("\n: ");
+
+            // pokud je input validní písmeno
+            if (!char.TryParse(Console.ReadLine(), out input) || !char.IsLetter(input))
+            {
+              Console.WriteLine("You must only use letters.");
+              continue;
             }
 
-            // pokud se splní podmínky, písmeno se přidá do listu uhádnutých písmen
-            guessedLetters.Add(input);
+            // převedení písmena na lowercase
+            input = char.ToLower(input);
+
+            // pokud je písmeno v listu možných písmen (všechna bez diakritiky)
+            if (!alphabet.ContainsKey(input))
+            {
+              Console.WriteLine("Only letters without accents are supported.");
+              continue;
+            }
+
+            // zaznamenání uhádnutí
+            alphabet[input]++;
+
+            // pokud hráč písmeno již hádal
+            if (guessedLetters.Contains(input))
+            {
+              Console.WriteLine("You already used this letter.");
+              continue;
+            }
+
+            // pokud hádané písmeno ve slově není, odebere 1 život
+            if (!word.Contains(input))
+            {
+              lives--;
+            }
+            break;
+          }
+
+          // pokud se splní podmínky, písmeno se přidá do listu uhádnutých písmen
+          guessedLetters.Add(input);
         }   
       }
       Console.WriteLine("There are no more words!\n\nThank you for playing. :))");
